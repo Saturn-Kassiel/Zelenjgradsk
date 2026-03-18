0b5e8e3bec8393869a35e2ea79e5b32941bbbb3f
Saturn-Kassiel <saturngroup2025@gmail.com>
2026-03-18 12:16:26 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var APPS_URL = 'https://script.google.com/macros/s/AKfycbxRVH1_dyfZXQUN-M_TEHYPH2sjrytVDY1YLalzGgAzgsgbH-qEuaicsELI_HYFtdTV/exec';
+var APPS_URL = 'https://script.google.com/macros/s/AKfycbwgV4paK4k-kKjA_AGBgsyyPAyFiAUttK1O3zmzaCShpY7-_qpCvDq8VleYuOBJSixq/exec';
