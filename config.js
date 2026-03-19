671fbfb69622f1ddb8ae4d589db8d8a11fd82746
Saturn-Kassiel <saturngroup2025@gmail.com>
2026-03-19 19:50:42 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 var APPS_URL = 'https://api.aparts-baltika.ru/api';
-var CHAT_URL = 'https://script.google.com/macros/s/AKfycbzC87vasHKUdd6cBOOl3KQC903zMLGQf9ead74W1lfo9Z3m0d7-kWCyCBC-wRUNR5R9/exec';
+var CHAT_URL = 'https://script.google.com/macros/s/AKfycbxMWhVXXAtzAi5tcaYB3YL2Zb-a0x3hTpG-YODKl57lrdBLNleygfzT0qgDMUl-IeWl/exec';
