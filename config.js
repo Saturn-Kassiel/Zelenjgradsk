299e94b919f6ff95274c324487a8e1d9bc90bd37
Saturn-Kassiel <saturngroup2025@gmail.com>
2026-03-19 19:20:31 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+var APPS_URL = 'https://api.aparts-baltika.ru/api';
+var CHAT_URL = 'https://script.google.com/macros/s/AKfycbzC87vasHKUdd6cBOOl3KQC903zMLGQf9ead74W1lfo9Z3m0d7-kWCyCBC-wRUNR5R9/exec';
