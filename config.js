6c3ccccf3f5acc96b7881a7f8cb09016eed8634c
Saturn-Kassiel <saturngroup2025@gmail.com>
2026-03-19 19:20:06 +0400

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-var APPS_URL = 'https://api.aparts-baltika.ru/api';
-var CHAT_URL = 'https://script.google.com/macros/s/AKfycbzgXfy-_vIRr22jx3F0cj0OLjSeNl3orz5o6tAv7h3cyBWzKQI2vLKjIUJ5sQRy8t9M/exec';
